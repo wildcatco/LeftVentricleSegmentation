85d1008378ba04cebf74deaff4487baf9bbb8e7f
konyul <konyul@gmail.com>
2021-12-07 16:05:44 +0900

    add scripts for train and test

---
+++ scripts/train_example.sh
@@ -0,0 +1,7 @@
+python train.py --dataset A2C\
+                --encoder resnet18\
+                --num-workers 4\
+                --batch-size 4\
+                --lr 0.001\
+                --wd 0.01\
+                --max-epochs 60
